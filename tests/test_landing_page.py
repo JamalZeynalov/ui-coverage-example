75df07b78bcc3cf2a56a41768b9dac56ec295180
Jamal <jamal.zeynalov@gmail.com>
2024-06-18 17:22:45 +0300

    Update tests with steps

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -2,50 +2,59 @@ import allure
 import pytest
 
 
-@allure.id("12869")
+@allure.id("1")
 @allure.title("Check Discovery Session Link")
 def test_discovery_session_link(ultimate_qa_app):
-    ultimate_qa_app.landing_page.open()
+    with allure.step("Open the landing page"):
+        ultimate_qa_app.landing_page.open()
 
     expected_link_start = "https://forms.clickup.com/2314027/"
 
-    actual_link = ultimate_qa_app.landing_page.discovery_session_link.get_attribute(
-        "href"
-    )
+    with allure.step("Get the actual link"):
+        actual_link = ultimate_qa_app.landing_page.discovery_session_link.get_attribute(
+            "href"
+        )
 
-    assert (
-        expected_link_start in actual_link
-    ), f"Expected link to start with {expected_link_start}, but got {actual_link}"
+    with allure.step("Check if the actual link starts with the expected link"):
+        assert (
+            expected_link_start in actual_link
+        ), f"Expected link to start with {expected_link_start}, but got {actual_link}"
 
 
-@allure.id("12870")
+@allure.id("2")
 @allure.title("Check View Courses Button")
 def test_view_courses_button(ultimate_qa_app):
-    ultimate_qa_app.landing_page.open()
+    with allure.step("Open the landing page"):
+        ultimate_qa_app.landing_page.open()
 
-    assert (
-        ultimate_qa_app.landing_page.is_view_courses_button_visible()
-    ), "View Courses button is not visible"
+    with allure.step("Check if the 'View Courses' button is visible"):
+        assert (
+            ultimate_qa_app.landing_page.is_view_courses_button_visible()
+        ), "View Courses button is not visible"
 
 
-@allure.id("12871")
+@allure.id("3")
 @allure.title("Check Top Courses")
 @pytest.mark.parametrize(
     "expected_course", ["Web Development", "Python", "UX Design", "HTML & CSS"]
 )
 def test_top_courses(ultimate_qa_app, expected_course: str):
     """Check if the expected course is in the top courses list"""
-    ultimate_qa_app.landing_page.open()
 
-    assert ultimate_qa_app.landing_page.top_courses.is_course_displayed(
-        expected_course
-    ), f"Expected course {expected_course} is not in the list"
+    with allure.step("Open the landing page"):
+        ultimate_qa_app.landing_page.open()
+
+    with allure.step("Check if the expected course is in the list"):
+        assert ultimate_qa_app.landing_page.top_courses.is_course_displayed(
+            expected_course
+        ), f"Expected course {expected_course} is not in the list"
 
 
-@allure.id("12872")
+@allure.id("4")
 @allure.title("Check Welcome Message")
 def test_welcome_message(ultimate_qa_app):
-    ultimate_qa_app.landing_page.open()
+    with allure.step("Open the landing page"):
+        ultimate_qa_app.landing_page.open()
 
     expected_welcome_title = "Learn to Code Websites, Apps & Games"
     expected_welcome_text = (
@@ -54,10 +63,11 @@ def test_welcome_message(ultimate_qa_app):
         "neque orci tempor nulla, id sagittis nisi ante nec arcu."
     )
 
-    actual_welcome_title = (
-        ultimate_qa_app.landing_page.welcome_title.text_content.strip()
-    )
-    actual_welcome_text = ultimate_qa_app.landing_page.welcome_text.text_content.strip()
+    with allure.step("Get the actual welcome title and text"):
+        actual_welcome_title = (
+            ultimate_qa_app.landing_page.welcome_title.text_content.strip()
+        )
+        actual_welcome_text = ultimate_qa_app.landing_page.welcome_text.text_content.strip()
 
     soft_assert_msg = ""
     soft_assert_result = True
@@ -75,4 +85,5 @@ def test_welcome_message(ultimate_qa_app):
             f"\nbut got \n'{actual_welcome_text}'\n"
         )
 
-    assert soft_assert_result, soft_assert_msg
+    with allure.step("Check if the actual welcome title and text match the expected ones"):
+        assert soft_assert_result, soft_assert_msg
